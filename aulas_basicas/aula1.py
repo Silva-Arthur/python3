20a71b3476059ab99889170eade63067f4a9b70b
Arthur Silva <pereiras.oarthur@gmail.com>
2024-10-21 23:20:51 -0300

    aula 01 - print e comentários

---
+++ aulas_basicas/aula1.py
@@ -0,0 +1,10 @@
+# comentários em py
+
+#escrever na tela
+print('Hello World!')
+
+#docString, o py interpreta mas pode ser usado como comentários grandes
+"""
+Meu texto faz isso
+é um teste de interpretação
+"""
